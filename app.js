71aa74e6dd5497fc5b4d1df3067b9ce8913c0470
MattB-GA-Boston <matt.brendzel@generalassemb.ly>
2015-06-26 09:43:26 -0400

    Adds function `sameLineDistance`, with tests.
    Calculates the distance between any two stops on a given line, as an absolute value.
    I also wrote two tests for each line, with different numbers of stops between the examples.

--- app.js
+++ app.js
@@ -41,10 +41,30 @@ var find = function(array, value){
   return -1;
 };
 
+var sameLineDistance = function(line, firstStop, secondStop){
+  // Find the indices of firstStop and secondStop, and subtract them.
+  // We only care about magnitude, not direction, so we'll take the absolute value.
+  var diff = find(line, firstStop) - find(line, secondStop);
+  if (diff < 0) {
+    diff *= -1;
+  }
+  return diff;
+  // Using a ternary operator
+  // return (diff > 0)? diff : -diff;
+};
+
+
 var tester = function() {
-  console.log('find')
+  console.log('find');
   console.log("* should return 2, actually returns " + find(['a','b','c'], 'c'));
   console.log("* should return -1, actually returns " + find(['a','b','c'], 'z'));
+  console.log('sameLineDistance');
+  console.log('* should return 1, actually returns ' + sameLineDistance(red, "South Station", "Park Street"));
+  console.log('* should return 4, actually returns ' + sameLineDistance(red, "Kendall", "Davis"));
+  console.log('* should return 1, actually returns ' + sameLineDistance(orange, "Forest Hills", "Back Bay"));
+  console.log('* should return 3, actually returns ' + sameLineDistance(orange, "State", "North Station"));
+  console.log('* should return 1, actually returns ' + sameLineDistance(green, "Arlington", "Copley"));
+  console.log('* should return 5, actually returns ' + sameLineDistance(green, "Copley", "Haymarket"));
 }
 
 tester();
