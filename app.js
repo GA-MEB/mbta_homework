c1dee4d74253eff50870f8e01831769256ddb14e
MattB-GA-Boston <matt.brendzel@generalassemb.ly>
2015-06-26 09:18:10 -0400

    Adds red, orange, and green lines
    Lists out all stations on those lines as strings. All three lines intersect at only one place, "Park Street", in spite of what the list of station names might imply.

---
+++ app.js
@@ -0,0 +1,33 @@
+// MBTA HOMEWORK
+// Create a representation of the MBTA, with multiple 'lines', in order to determine the number of 'stops' (in other words, the distance) between an origin and a destination, accounting for the possibility of transferring between different lines.
+
+var red, orange, green;   // Three lines, all intersecting at Park
+
+red = [
+  "South Station",
+  "Park Street",
+  "Kendall",
+  "Central",
+  "Harvard",
+  "Porter",
+  "Davis",
+  "Alewife"
+];
+orange = [
+  "North Station",
+  "Haymarket",
+  "Park Street",
+  "State",
+  "Downtown Crossing",
+  "Chinatown",
+  "Back Bay",
+  "Forest Hills"
+];
+green = [
+  "Haymarket",
+  "Government Center",
+  "Park Street",
+  "Boylston",
+  "Arlington",
+  "Copley"
+];
